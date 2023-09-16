d2aa0a179f7a38ca7fc80e51925780145f25900a
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2023-09-16 21:44:35 +1200

    4.3 Setting the Application Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'articles#index'
 
-  # Defines the root path route ("/")
-  root 'rails/welcome#index'
   get 'articles', to: 'articles#index'
 end
